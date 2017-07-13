4b0a02fae9525c33b1460f8dcfe2054763c858e9
ZHB <zhb020@gmail.com>
2017-07-13 16:33:44 +0800

    完善

--- src/main/java/com/nowui/chuangshi/cache/TradeCache.java
+++ src/main/java/com/nowui/chuangshi/cache/TradeCache.java
@@ -17,7 +17,6 @@ public class TradeCache extends Cache {
     public static final String TRADE_BY_TRADE_ID_CACHE = "trade_by_trade_id_cache";
     public static final String TRADE_BY_TRADE_NUMBER_CACHE = "trade_by_trade_number_cache";
     public static final String TRADE_NUMBER_LIST_CACHE = "trade_number_list_cache";
-    
 
     private TradeDao tradeDao = new TradeDao();
 
@@ -70,7 +69,7 @@ public class TradeCache extends Cache {
 
         return trade;
     }
-    
+
     public Trade findByTrade_number(String trade_number) {
         Trade trade = CacheUtil.get(TRADE_BY_TRADE_NUMBER_CACHE, trade_number);
 
@@ -83,17 +82,32 @@ public class TradeCache extends Cache {
         return trade;
     }
 
-    public Boolean save(String trade_id, String app_id, String user_id, String trade_number, String trade_receiver_name, String trade_receiver_mobile, String trade_receiver_province, String trade_receiver_city, String trade_receiver_area, String trade_receiver_address, String trade_message, Integer trade_product_quantity, BigDecimal trade_product_amount, BigDecimal trade_express_amount, BigDecimal trade_discount_amount, Boolean trade_is_commission, Boolean trade_is_confirm, Boolean trade_is_pay, String trade_flow, Boolean trade_status, String trade_audit_status, String system_create_user_id) {
-        return tradeDao.save(trade_id, app_id, user_id, trade_number, trade_receiver_name, trade_receiver_mobile, trade_receiver_province, trade_receiver_city, trade_receiver_area, trade_receiver_address, trade_message, trade_product_quantity, trade_product_amount, trade_express_amount, trade_discount_amount, trade_is_commission, trade_is_confirm, trade_is_pay, trade_flow, trade_status, trade_audit_status, system_create_user_id);
+    public Boolean save(String trade_id, String app_id, String user_id, String trade_number, String trade_receiver_name, String trade_receiver_mobile,
+            String trade_receiver_province, String trade_receiver_city, String trade_receiver_area, String trade_receiver_address,
+            String trade_message, Integer trade_product_quantity, BigDecimal trade_product_amount, BigDecimal trade_express_amount,
+            BigDecimal trade_discount_amount, Boolean trade_is_commission, Boolean trade_is_confirm, Boolean trade_is_pay, String trade_flow,
+            Boolean trade_status, String trade_audit_status, String system_create_user_id) {
+        return tradeDao.save(trade_id, app_id, user_id, trade_number, trade_receiver_name, trade_receiver_mobile, trade_receiver_province,
+                trade_receiver_city, trade_receiver_area, trade_receiver_address, trade_message, trade_product_quantity, trade_product_amount,
+                trade_express_amount, trade_discount_amount, trade_is_commission, trade_is_confirm, trade_is_pay, trade_flow, trade_status,
+                trade_audit_status, system_create_user_id);
     }
 
-    public Boolean updateValidateSystem_version(String trade_id, String user_id, String trade_number, String trade_receiver_name, String trade_receiver_mobile, String trade_receiver_province, String trade_receiver_city, String trade_receiver_area, String trade_receiver_address, String trade_message, Integer trade_product_quantity, BigDecimal trade_product_amount, BigDecimal trade_express_amount, BigDecimal trade_discount_amount, Boolean trade_is_commission, Boolean trade_is_confirm, Boolean trade_is_pay, String trade_flow, Boolean trade_status, String trade_audit_status, String system_update_user_id, Integer system_version) {
+    public Boolean updateValidateSystem_version(String trade_id, String user_id, String trade_number, String trade_receiver_name,
+            String trade_receiver_mobile, String trade_receiver_province, String trade_receiver_city, String trade_receiver_area,
+            String trade_receiver_address, String trade_message, Integer trade_product_quantity, BigDecimal trade_product_amount,
+            BigDecimal trade_express_amount, BigDecimal trade_discount_amount, Boolean trade_is_commission, Boolean trade_is_confirm,
+            Boolean trade_is_pay, String trade_flow, Boolean trade_status, String trade_audit_status, String system_update_user_id,
+            Integer system_version) {
         Trade trade = findByTrade_id(trade_id);
         if (!trade.getSystem_version().equals(system_version)) {
             throw new RuntimeException(Constant.ERROR_VERSION);
         }
 
-        boolean result = tradeDao.update(trade_id, user_id, trade_number, trade_receiver_name, trade_receiver_mobile, trade_receiver_province, trade_receiver_city, trade_receiver_area, trade_receiver_address, trade_message, trade_product_quantity, trade_product_amount, trade_express_amount, trade_discount_amount, trade_is_commission, trade_is_confirm, trade_is_pay, trade_flow, trade_status, trade_audit_status, system_update_user_id, system_version);
+        boolean result = tradeDao.update(trade_id, user_id, trade_number, trade_receiver_name, trade_receiver_mobile, trade_receiver_province,
+                trade_receiver_city, trade_receiver_area, trade_receiver_address, trade_message, trade_product_quantity, trade_product_amount,
+                trade_express_amount, trade_discount_amount, trade_is_commission, trade_is_confirm, trade_is_pay, trade_flow, trade_status,
+                trade_audit_status, system_update_user_id, system_version);
 
         if (result) {
             CacheUtil.remove(TRADE_BY_TRADE_ID_CACHE, trade_id);
@@ -103,7 +117,8 @@ public class TradeCache extends Cache {
         return result;
     }
 
-    public Boolean deleteByTrade_idAndSystem_update_user_idValidateSystem_version(String trade_id, String system_update_user_id, Integer system_version) {
+    public Boolean deleteByTrade_idAndSystem_update_user_idValidateSystem_version(String trade_id, String system_update_user_id,
+            Integer system_version) {
         Trade trade = findByTrade_id(trade_id);
         if (!trade.getSystem_version().equals(system_version)) {
             throw new RuntimeException(Constant.ERROR_VERSION);
@@ -118,14 +133,16 @@ public class TradeCache extends Cache {
 
         return result;
     }
-    
-    public Boolean updateTrade_is_payAndTrade_flowAndSystem_update_user_idAndSystem_update_timeAndByTrade_idAndSystem_version(String trade_id, Boolean trade_is_pay, String trade_flow, String system_update_user_id, Integer system_version) {
+
+    public Boolean updateTrade_is_payAndTrade_flowAndSystem_update_user_idAndSystem_update_timeAndByTrade_idAndSystem_version(String trade_id,
+            Boolean trade_is_pay, String trade_flow, String system_update_user_id, Integer system_version) {
         Trade trade = findByTrade_id(trade_id);
         if (!trade.getSystem_version().equals(system_version)) {
             throw new RuntimeException(Constant.ERROR_VERSION);
         }
 
-        boolean result = tradeDao.updateTrade_is_payAndTrade_flowAndSystem_update_user_idAndSystem_update_timeAndByTrade_idAndSystem_version(trade_id, trade_is_pay, trade_flow, system_update_user_id, system_version);
+        boolean result = tradeDao.updateTrade_is_payAndTrade_flowAndSystem_update_user_idAndSystem_update_timeAndByTrade_idAndSystem_version(trade_id,
+                trade_is_pay, trade_flow, system_update_user_id, system_version);
 
         if (result) {
             CacheUtil.remove(TRADE_BY_TRADE_ID_CACHE, trade_id);
@@ -134,26 +151,43 @@ public class TradeCache extends Cache {
 
         return result;
     }
-    
+
     /**
      * 生成订单号
+     * 
      * @return
      */
     public String generateTrade_number() {
-        String date = new SimpleDateFormat("yyyyMMdd").format(new Date());  
+        String date = new SimpleDateFormat("yyyyMMdd").format(new Date());
         List<String> trade_numberList = CacheUtil.get(TRADE_NUMBER_LIST_CACHE, date);
         String trade_number = new StringBuilder(date).append((new Random().nextInt(899999) + 10)).toString();
         if (trade_numberList == null) {
-            CacheUtil.removeAll(TRADE_NUMBER_LIST_CACHE); 
+            CacheUtil.removeAll(TRADE_NUMBER_LIST_CACHE);
             trade_numberList = new ArrayList<String>();
         } else {
-            while(trade_numberList.contains(trade_number)) {
+            while (trade_numberList.contains(trade_number)) {
                 trade_number = new StringBuilder(date).append((new Random().nextInt(899999) + 10)).toString();
             }
         }
         trade_numberList.add(trade_number);
         CacheUtil.put(TRADE_NUMBER_LIST_CACHE, date, trade_numberList);
         return trade_number;
+    }
+
+    public Boolean updateTrade_flowByTrade_idValidateSystem_version(String trade_id, String request_user_id, String system_version) {
+        Trade trade = findByTrade_id(trade_id);
+        if (!trade.getSystem_version().equals(system_version)) {
+            throw new RuntimeException(Constant.ERROR_VERSION);
+        }
+
+        boolean result = tradeDao.updateTrade_flowByTrade_idValidateSystem_version(trade_id, request_user_id, system_version);
+
+        if (result) {
+            CacheUtil.remove(TRADE_BY_TRADE_ID_CACHE, trade_id);
+            CacheUtil.remove(TRADE_BY_TRADE_NUMBER_CACHE, trade.getTrade_number());
+        }
+
+        return result;
     }
 
 }
