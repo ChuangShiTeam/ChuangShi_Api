27a7494add9961019678eeb5274d5282afcc28c8
ZHB <zhb020@gmail.com>
2017-08-02 14:02:43 +0800

    完善

--- src/main/java/com/nowui/chuangshi/util/DateUtil.java
+++ src/main/java/com/nowui/chuangshi/util/DateUtil.java
@@ -53,4 +53,24 @@ public class DateUtil {
         return todayEnd.getTime();
     }
 
+    public static Date getNowYearLastDay() {
+        Calendar cal = Calendar.getInstance();
+
+        // 设置年份
+        cal.set(Calendar.YEAR, cal.get(Calendar.YEAR));
+        // 设置月份
+        cal.set(Calendar.MONTH, 12 - 1);
+        // 获取某月最大天数
+        int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        // 设置日历中月份的最大天数
+        cal.set(Calendar.DAY_OF_MONTH, lastDay);
+        System.out.println("获取当前月的最后一天：" + cal.getTime());
+
+        // SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        // String lastDayOfMonth = sdf.format(cal.getTime());
+        // System.out.println("获取当前月的最后一天：" + lastDayOfMonth);
+
+        return cal.getTime();
+    }
+
 }
