fc04740afa3294b702e0ed6c704378e38f15706c
NowUI <nowui@163.com>
2017-09-14 20:09:34 +0800

    修改

--- src/main/java/com/nowui/chuangshi/util/MQUtil.java
+++ src/main/java/com/nowui/chuangshi/util/MQUtil.java
@@ -1,6 +1,7 @@
 package com.nowui.chuangshi.util;
 
 import com.nowui.chuangshi.rocket.Producer;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
@@ -8,17 +9,18 @@ import java.io.UnsupportedEncodingException;
 
 public class MQUtil {
 
-    public static void sendMessage(String tag, String message) {
+    public static SendResult sendMessage(String tag, String message) {
         try {
             Message msg = new Message("Topic",
                     tag,
                     "",
                     message.getBytes(RemotingHelper.DEFAULT_CHARSET));
 
-            Producer.instance.send(msg);
+            return Producer.instance.send(msg);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
+        return null;
     }
 }
