b4a9046ab49f822118bf761ca72ecb82048fdf59
marcus <marcus_hualebin@163.com>
2017-09-17 18:57:55 +0800

    概率工具类

---
+++ src/main/java/com/nowui/chuangshi/util/ProbabilityUtil.java
@@ -0,0 +1,35 @@
+package com.nowui.chuangshi.util;
+
+public class ProbabilityUtil {
+    
+    public static Boolean random(double percent) {
+        if (percent <= 0  || percent > 1 ) {
+            return false;
+        }
+        
+        if (percent == 1) {
+            return true;
+        }
+        double random = Math.random();
+        if (random <= percent) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public static void main(String args[]) {
+        int countTrue = 0;
+        int countFalse = 1;
+        for (int i = 0; i < 100000000; i++) {
+            if (random(0.80)) {
+                countTrue++; 
+            } else {
+                countFalse++;
+            }
+        }
+        System.out.println(countTrue);
+        System.out.println(countFalse);
+    }
+
+}
