d64a7a02d0796c23d94807d3ffda013f987a85c5
marcus <846079865@qq.com>
2017-10-26 10:04:43 +0800

    完善

--- src/main/java/com/nowui/chuangshi/api/uni/mobile/UniApplyController.java
+++ src/main/java/com/nowui/chuangshi/api/uni/mobile/UniApplyController.java
@@ -29,14 +29,14 @@ public class UniApplyController extends Controller {
         UniApply model = getModel(UniApply.class);
         model.setApply_id(Util.getRandomUUID());
         String request_user_id = getRequest_user_id();
-        /*String request_app_id = getRequest_app_id();
+        String request_app_id = getRequest_app_id();
         
         //判断手机号码是否报名过
         UniApply uniApply = UniApplyService.instance.mobileFind(request_app_id, model.getApply_mobile());
         
         if (uniApply != null) {
-            throw new RuntimeException("手机号码已经用过了");
-        }*/
+            throw new RuntimeException("该手机号码已经报名过了");
+        }
 
         Boolean result = UniApplyService.instance.save(model, request_user_id);
 
